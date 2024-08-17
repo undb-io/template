c3b1c5fd4ff7d49176bb86c42bf17e8bd24a5fc4
nichenqin <nichenqin@hotmail.com>
2024-08-17 22:40:52 +0800

    ui: update ui

--- src/lib/components/template/template-card.svelte
+++ src/lib/components/template/template-card.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-  import type { Template } from "$lib/api";
-  import * as Card from "$lib/components/ui/card";
-  import { AspectRatio } from "$lib/components/ui/aspect-ratio";
-  import { MoveRightIcon } from "lucide-svelte";
+  import type { Template } from "$lib/api"
+  import * as Card from "$lib/components/ui/card"
+  import { AspectRatio } from "$lib/components/ui/aspect-ratio"
+  import { MoveRightIcon } from "lucide-svelte"
 
-  export let template: Template;
+  export let template: Template
 </script>
 
 <a href={`/template/${template.id}`}>
   <Card.Root>
-    <Card.Content class="group flex min-h-[400px] flex-col p-0 shadow-sm transition-shadow hover:shadow-md">
+    <Card.Content class="group flex min-h-[300px] flex-col p-0 shadow-sm transition-shadow hover:shadow-md">
       {@const cover = template.values.Cover?.[0]}
       <AspectRatio ratio={16 / 9} class="bg-muted">
         {#if cover}
@@ -17,13 +17,17 @@
         {/if}
       </AspectRatio>
 
-      <div class="flex-1 px-4 py-1">
+      <div class="flex-1 px-4 pb-4 pt-3">
         <p class="flex items-center font-semibold">
           <span>
             {template.values.Title}
           </span>
 
           <MoveRightIcon class="ml-2 h-4 w-4 transition-transform group-hover:translate-x-1.5" />
+        </p>
+
+        <p class="line-clamp-6 text-sm text-muted-foreground">
+          {template.values.Summary}
         </p>
       </div>
     </Card.Content>
