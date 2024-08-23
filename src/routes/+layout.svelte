4a937bc401751b75fba5019e040ee7cae10e32f7
nichenqin <nichenqin@hotmail.com>
2024-08-23 14:15:54 +0800

    ui: add footer component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
-  import Header from "$lib/components/header/header.svelte";
-  import "../app.css";
+  import Header from "$lib/components/header/header.svelte"
+  import Footer from "$lib/components/footer/footer.svelte"
+  import "../app.css"
 </script>
 
-<div class="app">
+<div class="app flex min-h-screen flex-col">
   <Header />
-  <main>
+  <main class="flex-1">
     <slot></slot>
   </main>
+  <Footer />
 </div>
