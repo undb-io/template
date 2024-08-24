c47d78b92242e2cad086fb2ae85e5a187f34ddf8
nichenqin <nichenqin@hotmail.com>
2024-08-24 22:21:59 +0800

    feat: using @undb/js-sdk

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,15 +1,18 @@
 import { UNDB_API_KEY } from "$env/static/private"
 import { PUBLIC_UNDB_HOST } from "$env/static/public"
-import createClient from "openapi-fetch"
+import { Client } from "@undb/js-sdk"
 import type { components, paths } from "./templates"
 
 const headers = new Headers()
 headers.set("X-Undb-Api-Token", UNDB_API_KEY)
 headers.set("User-Agent", "undb-sveltekit-template")
 
-export const templateClient = createClient<paths>({
+export const undb = new Client({
   baseUrl: PUBLIC_UNDB_HOST + "/api",
-  headers,
 })
+
+undb.setSecret(UNDB_API_KEY)
+
+export const templateClient = undb.openapi<paths>()
 
 export type Template = components["schemas"]["grid:ViewRecord"]
